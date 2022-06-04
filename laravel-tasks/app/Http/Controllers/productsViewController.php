5d478b7c4f2427442d92790a1ea3003c6dcc91e5
Taebah shlouh <119taeba@gmail.com>
2022-06-04 20:23:50 +0300

    done

--- laravel-tasks/app/Http/Controllers/productsViewController.php
+++ laravel-tasks/app/Http/Controllers/productsViewController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
-
+use App\products;
 class productsViewController extends Controller
 { 
     public function productsRender(){
             $candy = DB::select('select * from candy');
             return view('/shop',['candy'=>$candy]);
-          }
+          } 
 }
